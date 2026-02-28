c2e0f59463dbdbed3d1c5de945fa0e259a3358bc
x4exr <rayyankhan1428h@gmail.com>
2026-02-27 22:01:38 -0500

    chore: setup a winit window with arc.

--- fxst-devtool/src/ui.rs
+++ fxst-devtool/src/ui.rs
@@ -1,37 +1,61 @@
 pub mod error;
 
+use std::sync::Arc;
+use wgpu::{BackendOptions, Backends, Instance, InstanceDescriptor, InstanceFlags, MemoryBudgetThresholds, Surface};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::platform::windows::EventLoopBuilderExtWindows;
-use winit::window::{Window, WindowId};
+use winit::window::{Window, WindowAttributes, WindowId};
 use crate::ui::error::{StartError, StartErrorTask};
 
-pub struct EventLoopData {
-
+pub struct EventLoopContext {
+    windows: Vec<Arc<Window>>,
+    surface: Surface
 }
 
-impl ApplicationHandler for EventLoopData {
+fn create_instance() -> Instance {
+    let backend_options = BackendOptions::default();
+
+    let desc = InstanceDescriptor {
+        backend_options,
+        flags: InstanceFlags::DEBUG,
+        backends: Backends::VULKAN,
+        memory_budget_thresholds: MemoryBudgetThresholds::default()
+    };
+
+    Instance::new(&desc)
+}
+
+impl ApplicationHandler for EventLoopContext {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        todo!()
+        let mut attrs = WindowAttributes::default();
+        attrs.title = String::from("FXST Devtool");
+        let window = Arc::new(event_loop.create_window(attrs).expect("Cant create window"));
+
+        let instance = create_instance();
+        let surface = instance.create_surface(window.clone()).expect("Cant create surface");
+
+        window.set_visible(true);
+        self.windows.push(window.clone());
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
-        todo!()
+
     }
 }
 
-impl EventLoopData {
+impl EventLoopContext {
     pub fn new() -> Self {
-        EventLoopData {}
+        EventLoopContext { windows: vec![] }
     }
 }
 
-fn start() -> Result<(), StartError> {
+pub fn start() -> Result<(), StartError> {
     let event_loop = EventLoop::new().map_err(|err| StartError::EventLoopError(err, StartErrorTask::CreateEventLoop))?;
     event_loop.set_control_flow(ControlFlow::Wait);
 
-    let mut data = EventLoopData::new();
+    let mut data = EventLoopContext::new();
     event_loop.run_app(&mut data).map_err(|err| StartError::EventLoopError(err, StartErrorTask::RunApp))?;
 
     Ok(())
